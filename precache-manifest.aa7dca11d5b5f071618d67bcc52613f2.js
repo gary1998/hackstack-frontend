782c87f4a3bc8de6f76d3a2576a72420cc45109f
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-25 11:33:40 +0530

    Updates

---
+++ precache-manifest.aa7dca11d5b5f071618d67bcc52613f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19acb51210f357f848e6b3eae0ce53b",
+    "url": "/hackstack-frontend/index.html"
+  },
+  {
+    "revision": "1474ab5a84553379d432",
+    "url": "/hackstack-frontend/static/css/main.65d1317d.chunk.css"
+  },
+  {
+    "revision": "9f4317622b50b837b619",
+    "url": "/hackstack-frontend/static/js/2.51c9e7df.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/hackstack-frontend/static/js/2.51c9e7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1474ab5a84553379d432",
+    "url": "/hackstack-frontend/static/js/main.f9226a44.chunk.js"
+  },
+  {
+    "revision": "971d7a6fa4b1edda158d",
+    "url": "/hackstack-frontend/static/js/runtime-main.3cd17deb.js"
+  }
+]);
